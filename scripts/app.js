dac9cd8e9cdc8ae32b484d1f36634490b692b443
wre0rl <wre0rl@gmail.com>
2023-06-04 03:48:02 +0700

    Add player constructor and modified Game logic

--- scripts/app.js
+++ scripts/app.js
@@ -3,31 +3,59 @@ const Gameboard = (function() {
   const columns = 3;
   const board = [];
 
-  function create() {
-    for (let i = 0; i < rows; i++) {
-      board[i] = [];
-      for (let j = 0; j < columns; j++) {
-        board[i].push(j);
-      }
+  // Create
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(0);
     }
   }
 
-  function get() {
-    return board;
+  const get = () => board;
+
+  const mark = (player, row, column) => {
+    if (board[row][column] !== 0) {
+      return console.log('That square is marked!');
+    }
+    return board[row][column] = player;
   }
 
   return {
-    create,
-    get
+    get,
+    mark
   };
 })();
 
-// 0 empty, 1 player one, 2 player 2
-const Square = (function() {
-  let value = 0;
+function Player(name, mark, isActive) {
+  this.name = name;
+  this.mark = mark; // 1 = X, 2 = O
+}
 
-  
+const Game = (function() {
+  const player1 = new Player('Bob', 1);
+  const player2 = new Player('John', 2);
+  const players = [player1, player2];
+
+  let activePlayer = players[0];
+
+  const play = (row, column) => {
+    const mark = Gameboard.mark(activePlayer.mark, row, column);
+    if (mark === activePlayer.mark) switchActivePlayer();
+    return Gameboard.get();
+  }
+
+  const switchActivePlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  }
+
+  const getActivePlayer = () => activePlayer;
+
+  const getPlayers = () => players;
+
+  return {
+    play,
+    getPlayers,
+    getActivePlayer
+  };
 })();
 
-Gameboard.create();
-console.log(Gameboard.get());
