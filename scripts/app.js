94dc44eb0f0da88b6bd2fd2aa90cd6483c8793a8
wre0rl <wre0rl@gmail.com>
2023-06-04 07:36:14 +0700

    Add a simple and ugly check winner logic

--- scripts/app.js
+++ scripts/app.js
@@ -30,8 +30,8 @@ function Player(name, mark) {
 }
 
 const Game = (function() {
-  const player1 = new Player('Bob', 'X');
-  const player2 = new Player('John', 'O');
+  const player1 = new Player('Player 1', 'X');
+  const player2 = new Player('Player 2', 'O');
   const players = [player1, player2];
 
   let activePlayer = players[0];
@@ -39,13 +39,47 @@ const Game = (function() {
   const play = (row, column) => {
     const currentPlayerMark = getActivePlayer().mark;
     const squareMarked = Gameboard.markSquare(currentPlayerMark, row, column);
-    console.log(squareMarked);
-    return squareMarked ? (switchActivePlayer(), Gameboard.getBoard()) : Gameboard.getBoard();
+    // Refactor the check winner logic, and check draw
+    const a = checkRow(row, squareMarked);
+    const b = checkColumn(column, squareMarked);
+    const c = checkDiagonal(squareMarked);
+    a || b || c ? console.log(`${getActivePlayer().name} is the winner`) : null;
+    return squareMarked ? (switchActivePlayer(), Gameboard.getBoard()) : Gameboard.getBoard(); // Remove this...
   }
+
+  const checkRow = (row, squareMarked) => {
+    const board = Gameboard.getBoard();
+    return board[row].every((col) => col === squareMarked);
+  };
+
+  const checkColumn = (column, squareMarked) => {
+    const board = Gameboard.getBoard();
+    let temp = [];
+    board.forEach((row) => {
+      temp.push(row[column]);
+    });
+    return temp.every((col) => col === squareMarked);
+  };
+
+  const checkDiagonal = (squareMarked) => {
+    let board = Gameboard.getBoard();
+    let leftDiag = [];
+    let leftCol = 0;
+    let rightDiag = [];
+    let rightCol = 2;
+    board.forEach((row) => {
+      leftDiag.push(row[leftCol++]);
+      rightDiag.push(row[rightCol--]);
+    });
+
+    const isWinLeft = leftDiag.every((col) => col === squareMarked);
+    const isWinRight = rightDiag.every((col) => col === squareMarked);
+    return isWinLeft || isWinRight ? true : false;
+  };
 
   const switchActivePlayer = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
-  }
+  };
 
   const getActivePlayer = () => activePlayer;
 
